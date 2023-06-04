4fe04501c577dcf7ee679e406fda51027e76867c
zanozano <zanozanozano@gmail.com>
2023-06-04 13:35:46 +0700

    blog terminado

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,7 +36,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+        format.html { redirect_to post_url(@post), notice: "Nuevo Post creado" }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -49,7 +49,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
+        format.html { redirect_to post_url(@post), notice: "Post Actualizado" }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -63,7 +63,7 @@ class PostsController < ApplicationController
     @post.destroy
 
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+      format.html { redirect_to posts_url, notice: "Post Eliminado" }
       format.json { head :no_content }
     end
   end
@@ -77,7 +77,7 @@ class PostsController < ApplicationController
           # Incrementar el contador de likes en la columna likes_count del post
           @post.increment!(:likes_count)
 
-          format.html { redirect_to post_url(@post), notice: "Like was successfully created." }
+          format.html { redirect_to post_url(@post), notice: "Nuevo like" }
           format.json { render :show, status: :created, location: @post }
         else
           format.html { redirect_to post_url(@post), alert: "Failed to create like." }
